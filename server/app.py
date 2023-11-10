39e87f661782e752db8b1c31c7899257a5d81d46
Olayanju Oluwadamilare <hadreytechsolutions@gmail.com>
2023-11-10 15:35:27 +0100

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -20,19 +20,72 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    
+    bakeries = []
+    for bakery in BakedGood.query.all():
+        bakery_dict = {
+            "name": bakery.name,
+            "price": bakery.price,
+            "id": bakery.id,
+            "updated_at": bakery.updated_at,
+            "created_at": bakery.created_at,
+            "bakery_id": bakery.bakery_id
+        }
+        bakeries.append(bakery_dict)
+        
+        response = make_response(
+            bakeries, 
+            200,
+            {"Content-Type": "application/json"}
+            )
+        
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(
+        # it still needs to be JSON, after all
+        jsonify(bakery_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    bakery = BakedGood.query.order_by(BakedGood.price.desc()).all()
+
+    bakery_dict = [bakery_good.to_dict() for bakery_good in bakery]
+ 
+    response = make_response(
+        # it still needs to be JSON, after all
+        jsonify(bakery_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price).first()
+     
+    baked_good = baked_good.to_dict()
+     
+    response = make_response(
+        # it still needs to be JSON, after all
+        jsonify(baked_good),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
